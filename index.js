8490930db7888eb535acc6cbd278fc00ac0f84b9
evlbe <150168123+evlbe@users.noreply.github.com>
2025-03-03 15:30:56 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        hey: "there",
+        hey2: "there2",
+    })
+});
+app.listen(3000);
